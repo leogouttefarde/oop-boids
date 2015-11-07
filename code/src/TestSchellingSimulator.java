aca2e6831765c95e116d867653e4f0442f86f035
Ilyes Kacher <ilyes.kacher@ensimag.grenoble-inp.fr>
2015-11-07 16:23:35 +0100

    Ajout de couleur aléatoire dans Schelling

--- code/src/TestSchellingSimulator.java
+++ code/src/TestSchellingSimulator.java
@@ -6,7 +6,16 @@ public class TestSchellingSimulator {
 
 	public static void main(String[] args) {
 		
-		int numberOfColor = 3;
+		int numberOfColor = 7;
+		Color color[] = new Color[numberOfColor + 1];
+		color[0] = new Color(255, 255, 255);
+		for(int i = 1; i < color.length; i++){
+			int r = ThreadLocalRandom.current().nextInt(0, 255);
+			int g = ThreadLocalRandom.current().nextInt(0, 255);
+			int b = ThreadLocalRandom.current().nextInt(0, 255);
+			color[i] = new Color(r, g, b);
+		}
+		
 		int threshold = 3;
 		int n = 50;
 		int m = 50;
@@ -32,7 +41,7 @@ public class TestSchellingSimulator {
 		
 		GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
 
-		gui.setSimulable(new SchellingSimulator(gui, sch));
+		gui.setSimulable(new SchellingSimulator(gui, sch, color));
 	}
 
 }
