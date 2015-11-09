2f95a64136694ef882dddd16ad7fbdb023a1f3e7
Nejmeddine DOUMA <nejmeddine.douma@ensimag.grenoble-inp.fr>
2015-11-09 01:44:06 +0100

    Event Manager

---
+++ code/src/EventManager.java
@@ -0,0 +1,40 @@
+import java.util.PriorityQueue;
+
+public class EventManager {
+	
+	private long currentDate;
+	
+	private PriorityQueue<Event> events;
+	
+	public EventManager(){
+		events = new PriorityQueue<Event>();
+		currentDate = 1;
+	}
+	
+	public void addEvent(Event e){
+		events.add(e);
+	};
+	
+	public void next(){
+		System.out.println("Next... Current date : "+currentDate);
+		Event d=events.poll();
+		while (d != null && d.getDate() <= currentDate){
+			d.execute();
+			d=events.poll();
+		}
+		if (d != null){
+			events.add(d);
+		}
+		currentDate++;
+	};
+	
+	public boolean isFinished(){
+		return events.isEmpty();
+	};
+	
+	public void restart(){
+		currentDate=1;
+		events.clear();
+	};
+	
+}
---
+++ code/src/TestEventManager.java
@@ -0,0 +1,23 @@
+public class TestEventManager {
+	public static void main(String[] args) throws InterruptedException{
+		// On crée un simulateur
+		EventManager manager = new EventManager();
+		
+		// On poste un événement [PING] tous les deux pas de temps
+		for (int i=2; i<=10; i+=2){
+			manager.addEvent(new MessageEvent(i, " [PING]"));
+			
+		}
+		
+		// On poste un événement [PONG] tous les trois pas de temps
+		for (int i=3; i <= 9; i +=3){
+			manager.addEvent(new MessageEvent(i, " [PONG]"));
+		}
+		
+		while (!manager.isFinished()){
+			manager.next();
+			Thread.sleep(1000);
+		}
+	}
+
+}
