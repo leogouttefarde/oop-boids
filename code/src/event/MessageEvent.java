cf43600b11754da0a349a6aca6b6b1635ebbd64d
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2015-11-15 20:52:26 +0100

    Javadoc Event

--- code/src/event/MessageEvent.java
+++ code/src/event/MessageEvent.java
@@ -1,14 +1,28 @@
 package event;
 
+/**
+ * Evénement affichant un message
+ * 
+ * @author Ilyes Kacher, Léo Gouttefarde, Nejmeddine Douma
+ */
 public class MessageEvent extends Event {
 
 	private String message;
 
+	/**
+	 * Crée un événement affichant un message à la date donnée.
+	 * 
+	 * @param date		Date
+	 * @param message	Message
+	 */
 	public MessageEvent(int date, String message) {
 		super(date);
 		this.message = message;
 	}
 
+	/* (non-Javadoc)
+	 * @see event.Event#execute()
+	 */
 	public void execute() {
 		System.out.println(this.getDate() + this.message);
 	}
